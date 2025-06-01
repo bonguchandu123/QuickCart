23620d479b605206b50a2b4318b6b227c3bdf73f
Your Name <your-email@example.com>
2025-06-01 19:23:01 +0530

    seoiijfskj

--- models/Address.js
+++ models/Address.js
@@ -1,23 +1,18 @@
 import mongoose from "mongoose";
 
-
-
 const addressSchema = new mongoose.Schema({
-    userId:{type:String,required:true},
-    fullName:{type:String,required:true},
-    
-    phoneNumber:{type:String,required:true},
-    pincode:{type:Number,required:true},
-  
-    area:{type:String,required:true},
-    city:{type:String,required:true},
-    state:{type:String,required:true},
-   
+  userId: { type: String, required: true },
+  fullName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  pincode: { type: Number, required: true },
+  area: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+}, {
+  timestamps: true,
+});
 
+// ✅ Capital "A" model name
+const Address = mongoose.models.Address || mongoose.model("Address", addressSchema);
 
-})
-
-const Address = mongoose.models.Address || mongoose.model('Address', addressSchema);
-
-
-export default Address
+export default Address;
