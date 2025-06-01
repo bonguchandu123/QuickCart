38e8a34d6e3dd1db5b22eede532ed64c1018b155
Your Name <your-email@example.com>
2025-06-01 18:48:44 +0530

    new error is gandeld

--- app/api/slidder/get/route.js
+++ app/api/slidder/get/route.js
@@ -2,17 +2,20 @@ import { getAuth } from "@clerk/nextjs/server";
 import authSeller from "@/lib/authSeller";
 import Slidder from "@/models/Slidder";
 import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
 
 
 export async function GET(request) {
 
     try {
         const {userId} = getAuth(request)
+
       
         const isSeller = await authSeller(userId)
         if(!isSeller){
             return NextResponse.json({success:false,message:"not authorized"})
         }
+        await connectDB();
         const slidders = await Slidder.find({})
 
         return NextResponse.json({success:true,slidders})
